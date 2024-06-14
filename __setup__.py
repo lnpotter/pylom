af77a57b626cf3fa52675a0dd4367f21da271049
LordLucass <lucasnoalpottergithub@gmail.com>
2024-06-14 16:37:41 -0300

    Adding information for download.

---
+++ __setup__.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+      name="Pylom",
+      description="A simple and easy-to-use Python log library.",
+      author="lnpotter",
+      url="https://github.com/lnpotter/pylom",
+      version="1.0.0",
+      packages=["pylom"],
+      python_requires="3.11",
+      license="MIT"
+)
